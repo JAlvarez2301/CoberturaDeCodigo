5a6537b5ab175304714f0d29e75ba75d18766de6
JAlvarez2301 <124701952+JAlvarez2301@users.noreply.github.com>
2023-04-25 17:24:44 +0200

    Update testPar_Impar.py

--- testPar_Impar.py
+++ testPar_Impar.py
@@ -15,4 +15,4 @@ def test_suma_impares():
 
 def test_cuadrado_lista():
     listaNum = par_Impar([1,2,3,4,5,6,7,8,9,10])
-    assert listaNum.cuadradoLista() == [1,4,9,16,25,36,49,64,81,100]
+    assert listaNum.cuadrado_lista() == [1,4,9,16,25,36,49,64,81,100]
