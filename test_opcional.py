545ba677276089ca8a6ffc9d62e20d32b7f71904
LAPTOP-QRN2VF75\Usuario <javieralvarez2397@gmail.com>
2023-05-09 18:03:22 +0200

    Lo volvemos a intentar

--- test_opcional.py
+++ test_opcional.py
@@ -1,12 +1,12 @@
-from opcional import opcional
+from opcional import practicaOpcional
 
 
 def test_contar_vocales():
-    cadena = opcional("Vamos a comprobar que funciona la clase opcional.py")
-    assert cadena.contarVocales() == 19
+    cadena = practicaOpcional("Vamos a comprobar que funciona la clase opcional.py")
+    assert cadena.contar_vocales() == 19
 
 
 def test_contar_palabras():
-    cadena = opcional(
+    cadena = practicaOpcional(
         "Ahora, vamos a comprobar que funciona la clase opcional, así que venga, cuantas palabras tiene esta oracion campeón")
-    assert cadena.contarPalabras() == 18
+    assert cadena.contar_palabras() == 18
